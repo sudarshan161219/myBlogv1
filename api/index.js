8da87d8d4b18bb01fed06b78d1b286a2bf536c85
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-02-27 13:29:17 +0530

    updata

--- api/index.js
+++ api/index.js
@@ -3,27 +3,66 @@ const app = express();
 const cors = require('cors')
 const connectDB = require('./db/connect')
 require("dotenv").config();
-const userModel = require('./models/user')
+const bcrypt = require('bcryptjs');
+const userModel = require('./models/user');
+const jwt = require('jsonwebtoken');
+
+const privateKey = 'randomString'
+const salt = bcrypt.genSaltSync(10)
+
 
 const port = process.env.PORT || 4000
-app.use(cors())
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 
 
 // data coming from Client --> src --> components --> register.jsx
+// adding users username and password to database
 app.post('/register', async (req, res) => {
     try {
         const { username, password } = req.body
         const userDoc = await userModel.create({
             username,
-            password
+            password: bcrypt.hashSync(password, salt)
         })
         res.json(userDoc)
     } catch (error) {
-        res.status(400).json({msg: {error}})
+        res.status(400).json({ msg: { error } })
     }
 
 })
+
+// data coming from Client --> src --> components --> login.jsx
+// checking users username and password from database 
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const userDoc = await userModel.findOne({ username })
+        const passOk = bcrypt.compareSync(password, userDoc.password)
+        res.json(passOk)
+        if (passOk) {
+            // user logged in
+            // res.json(passOk)
+            jwt.sign({ username, id: userDoc._id }, privateKey, {}, (err, token) => {
+                if (!err) {
+                    res.cookie('token', token).json('ok')
+                } else {
+                    console.log(err);
+                }
+            })
+        } else {
+            res.status(400).json({ msg: 'bad user credentials' })
+        }
+    } catch (error) {
+        res.status(400).json({ msg: { error } })
+    }
+})
+
 
 
 const start = async () => {
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,12 +1,64 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {Navigate} from 'react-router-dom'
+import { toast } from "react-toastify";
 const login = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUserName("");
+    setPassword("");
+
+    try {
+      const response = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "content-Type": "application/json" },
+        credentials: "include",
+      });
+
+      if (response.status === 200) {
+        setRedirect(true);
+      } else {
+        toast.error(`Opps!!, Login failed`, {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUserName = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handleUserPassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+if(redirect) {
+return <Navigate to={'/'} />
+}
+
   return (
     <main>
-      <form>
-      <h1>Login</h1>
-        <input type='text' placeholder='username' />
-        <input type='password' placeholder='password' />
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <input
+          type='text'
+          placeholder='username'
+          value={username}
+          onChange={handleUserName}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={handleUserPassword}
+        />
         <div className='btn-container'>
           <button className='button-28' role='button'>
             Login
